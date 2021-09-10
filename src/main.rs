00319aee8577bca3e41ecc84e7b24d15bbdc8b92
Mattias Päivärinta <mattias@paivarinta.se>
2021-09-11 01:19:21 +0200

    Event tree WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,60 @@
 pub mod outline;
 
+use rand::Rng;
+
+trait Action {
+    type Item;
+    fn apply(&self, heads: &mut Vec<Tree<Self::Item>>);
+}
+
+enum Tree<T> {
+    Event(T, Box<Tree<T>>),
+    Branch(Vec<Tree<T>>),
+}
+
+impl<T> Tree<T> {
+    pub fn from_actions<R, A, W>(rng: &mut R, max_heads: usize, actions: &[A], w: W) -> Tree<T>
+    where
+        R: Rng,
+        W: Fn(&Tree<T>) -> Box<dyn Fn(&Tree<T>) -> u32>,
+        A: Action<Item = T>,
+    {
+        use rand::distributions::weighted::WeightedIndex;
+        use rand::distributions::Distribution;
+
+        let mut heads = Vec::new();
+        for action in actions {
+            while heads.len() > max_heads {
+                let head1 = heads.remove(rng.gen_range(0..heads.len()));
+                let weighter = w(&head1);
+                let dist = WeightedIndex::new(heads.iter().map(weighter)).unwrap();
+                let head2 = heads.remove(dist.sample(rng));
+                heads.push(head1.join(head2));
+            }
+            action.apply(&mut heads);
+        }
+        Tree::Branch(heads)
+    }
+
+    pub fn join(self, other: Self) -> Self {
+        match (self, other) {
+            (Tree::Branch(mut u), Tree::Branch(mut v)) => {
+                u.extend(v.drain(..));
+                Tree::Branch(u)
+            }
+            (Tree::Branch(mut u), e) => {
+                u.push(e);
+                Tree::Branch(u)
+            }
+            (e, Tree::Branch(mut u)) => {
+                u.push(e);
+                Tree::Branch(u)
+            }
+            (e, f) => Tree::Branch(vec![e, f]),
+        }
+    }
+}
+
 fn main() {
     use outline::OutlineConf;
     use outline::Treasure;
@@ -17,7 +72,9 @@ fn main() {
     let actions = outline.action_sequence(&mut rng);
 
     //outline.show();
-    for action in actions {
+    for action in &actions {
         println!("{:?}", action);
     }
+
+    let tree = Tree::from_actions(&mut rng, 4, &actions, |_| Box::new(|_| 0));
 }
