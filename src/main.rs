3a06efae1f0ced89a3845d5d86ce7975e7fb68b4
Mattias Päivärinta <mattias@paivarinta.se>
2021-09-11 00:01:38 +0200

    Tidy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-mod outline;
+pub mod outline;
 
 fn main() {
-    use outline::Outline;
     use outline::OutlineConf;
     use outline::Treasure;
 
     let mut rng = rand::rngs::mock::StepRng::new(1, 3);
     //let mut rng = rand::rngs::ThreadRng::default();
 
-    let outline: Outline = OutlineConf {
+    let outline = OutlineConf {
         num_fairies: 1,
         num_cul_de_sacs: 1,
         num_small_keys: 2,
         treasures: [Treasure::BombsCounter].iter().cloned().collect(),
     }
-    .into();
+    .into_outline();
     let actions = outline.action_sequence(&mut rng);
 
     //outline.show();
