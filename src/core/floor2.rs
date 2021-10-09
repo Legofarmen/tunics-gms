b0d766337fbd55186e6de612bb35820b64fc5729
Mattias Päivärinta <mattias@paivarinta.se>
2021-10-09 18:55:10 +0200

    WIP

--- src/core/floor2.rs
+++ src/core/floor2.rs
@@ -1,3 +1,4 @@
+use crate::core::floor::FloorPlan;
 use crate::core::room::Forest;
 use crate::core::room::RoomExt;
 use crate::core::room::Tree;
@@ -5,12 +6,17 @@ use std::collections::BTreeSet;
 use std::ops::RangeInclusive;
 
 pub struct Level<T> {
-    depth: isize,
+    depth: i8,
     cells: Vec<T>,
 }
 
+impl<T> Level<T> {
+    pub fn set(&mut self, pos: i8, elem: T) {
+        self.cells[(self.depth + pos) as usize] = elem;
+    }
+}
 impl<D, C> Level<Forest<D, C>> {
-    pub fn new_forests(depth: isize) -> Self {
+    pub fn new_forests(depth: i8) -> Self {
         let width = 2 * depth as usize + 1;
         let mut forests = Vec::with_capacity(width);
         forests.resize_with(width, || Forest::new());
@@ -19,7 +25,7 @@ impl<D, C> Level<Forest<D, C>> {
             cells: forests,
         }
     }
-    pub fn is_empty(&self, pos: isize) -> bool {
+    pub fn is_empty(&self, pos: i8) -> bool {
         self.cells[(self.depth + pos) as usize].is_empty()
     }
 }
@@ -27,7 +33,7 @@ impl<D, C> Level<Forest<D, C>>
 where
     Tree<D, C>: RoomExt,
 {
-    pub fn score(&self, pos: isize) -> (isize, isize) {
+    pub fn score(&self, pos: i8) -> (isize, isize) {
         let forest = &self.cells[(self.depth + pos) as usize];
         let weight = forest.weight() as isize;
         let x = if weight > 1 {
@@ -39,27 +45,41 @@ where
     }
 }
 impl<T: Ord> Level<BTreeSet<T>> {
-    pub fn new_sets(depth: isize) -> Self {
+    pub fn new_sets(depth: i8) -> Self {
         let width = 2 * depth as usize + 1;
         let mut sets = Vec::with_capacity(width);
         sets.resize_with(width, || BTreeSet::new());
         Level { depth, cells: sets }
     }
-    pub fn insert(&mut self, pos: isize, elem: T) {
+    pub fn insert(&mut self, pos: i8, elem: T) {
         self.cells[(self.depth + pos) as usize].insert(elem);
     }
 }
 
-fn allocate<D, C>(next_level: &mut Level<Forest<D, C>>, forest: Forest<D, C>, pos: usize, dir: Dir3)
-where
+fn allocate<D, C>(
+    next_level: &mut Level<Forest<D, C>>,
+    forest: Forest<D, C>,
+    x: i8,
+    y: i8,
+    dir: Dir3,
+    floor_plan: &mut FloorPlan,
+) where
     Tree<D, C>: RoomExt,
 {
-    let delta = dir.delta();
+    let delta = match dir {
+        Dir3::North => 0,
+        Dir3::East => 1,
+        Dir3::West => -1,
+    };
     let forest = match forest.pop_tree() {
-        Ok((door, contents, forest)) => forest,
+        Ok((door, contents, forest)) => {
+            floor_plan.add_door((x, y, dir), door);
+            floor_plan.add_room((x, y), contents);
+            forest
+        }
         Err(forest) => forest,
     };
-    next_level.cells[pos + (delta + 1) as usize] = forest;
+    next_level.set(x + delta, forest);
 }
 
 pub fn doit<D, C, T: Ord>()
@@ -67,6 +87,7 @@ where
     Tree<D, C>: RoomExt,
 {
     let depth = 0;
+    let mut floor_plan = FloorPlan::default();
 
     let mut level = Level::<Forest<D, C>>::new_forests(depth);
 
@@ -84,21 +105,65 @@ where
             .zip(alloc.cells.into_iter())
             .enumerate()
         {
+            let x = i as i8 - depth;
+            let y = depth - x.abs();
             if alloc.len() == 0 {
             } else if alloc.len() == 1 {
                 let mut alloc = alloc.into_iter();
-                allocate(&mut next_level, forest, i, alloc.next().unwrap());
+                allocate(
+                    &mut next_level,
+                    forest,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
             } else if alloc.len() == 2 {
                 let mut alloc = alloc.into_iter();
                 let (forest1, forest2) = forest.split2();
-                allocate(&mut next_level, forest1, i, alloc.next().unwrap());
-                allocate(&mut next_level, forest2, i, alloc.next().unwrap());
+                allocate(
+                    &mut next_level,
+                    forest1,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
+                allocate(
+                    &mut next_level,
+                    forest2,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
             } else if alloc.len() == 3 {
                 let mut alloc = alloc.into_iter();
                 let (forest1, forest2, forest3) = forest.split3();
-                allocate(&mut next_level, forest1, i, alloc.next().unwrap());
-                allocate(&mut next_level, forest2, i, alloc.next().unwrap());
-                allocate(&mut next_level, forest3, i, alloc.next().unwrap());
+                allocate(
+                    &mut next_level,
+                    forest1,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
+                allocate(
+                    &mut next_level,
+                    forest2,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
+                allocate(
+                    &mut next_level,
+                    forest3,
+                    x,
+                    y,
+                    alloc.next().unwrap(),
+                    &mut floor_plan,
+                );
             } else {
                 unreachable!();
             }
@@ -116,31 +181,25 @@ enum Dir3 {
 }
 
 impl Dir3 {
-    fn delta(self) -> isize {
-        match self {
-            Dir3::North => 0,
-            Dir3::East => 1,
-            Dir3::West => -1,
-        }
-    }
+    fn delta(self) -> i8 {}
 }
 
 fn alloc_half<D, C>(
     level: &Level<Forest<D, C>>,
     alloc: &mut Level<BTreeSet<Dir3>>,
-    range: RangeInclusive<isize>,
+    range: RangeInclusive<i8>,
 ) -> bool
 where
     Tree<D, C>: RoomExt,
 {
     struct Best {
-        pos: isize,
+        pos: i8,
         score: (isize, isize),
     }
     let mut best: Option<Best> = None;
     let mut seen_non_empty = false;
     for pos in range {
-        let pos = pos as isize;
+        let pos = pos as i8;
         if !level.is_empty(pos) {
             seen_non_empty = true;
             if let Some(ref mut best) = &mut best {
